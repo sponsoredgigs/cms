4e391198380aa87f8ae62f74ce8e6ed38e115811
yurisasc <yuris_aryansyah@yahoo.com>
2023-09-06 00:11:25 +1000

    fix(validation): URL validation to include more symbols

--- src/validators/isUrl.ts
+++ src/validators/isUrl.ts
@@ -1,6 +1,8 @@
 export const isUrl = (value: string) => {
   if (
-    !value?.match(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/)
+    !value?.match(
+      /^(https?:\/\/)?([\da-zA-Z.-]+)\.([a-zA-Z]{2,6})(:\d+)?(\/[\w .-]*)*(\?[=&\w%.-]*)?(#[\w%&=.?-]*)?$/
+    )
   ) {
     return "Please enter a valid URL";
   }
